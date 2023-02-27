8b9bac98cf150e4982f0fb63922b828744f8fdbc
SumnDaiy <87270976+SumnDaiy@users.noreply.github.com>
2023-02-27 08:17:51 +0100

    feat(config): Multi blip, new tree locations

--- config.lua
+++ config.lua
@@ -15,158 +15,64 @@ Config.PriceMultiplier = 100 -- 1 log * 100 = 100$
 
 Config.Locations = {
     {
-        model = Config.treeModel,
-        location = vector3(-721.5167, 5383.3764, 55.006299),
-        rotation = vector3(-13, 0, 0),
+    model = Config.treeModel,
+    location = vec3(-728.488, 5401.086, 50.876),
+    rotation = vec3(0,0,0)
     },
     {
-        model = Config.treeModel,
-        location = vector3(-712.8502, 5374.6362, 58.041345),
-        rotation = vector3(-13, 0, 30),
+    model = Config.treeModel,
+    location = vec3(-716.452, 5395.412, 53.958),
+    rotation = vec3(0,0,0)
     },
     {
-        model = Config.treeModel,
-        location = vec3(-756.092, 5231.580, 99.844),
-        rotation = vector3(-13, 0, 30),
+    model = Config.treeModel,
+    location = vec3(-711.504, 5388.111, 56.417),
+    rotation = vec3(0,0,0)
     },
     {
-        model = Config.treeModel,
-        location = vec3(-764.002, 5239.331, 98.857),
-        rotation = vector3(-13, 0, 30),
+    model = Config.treeModel,
+    location = vec3(-720.513, 5387.617, 55.978),
+    rotation = vec3(0,0,0)
     },
     {
-        model = Config.treeModel,
-        location = vec3(-797.234, 5216.629, 106.238),
-        rotation = vector3(-13, 0, 30),
+    model = Config.treeModel,
+    location = vec3(-734.543, 5386.100, 54.204),
+    rotation = vec3(0,0,0)
     },
     {
-        model = Config.treeModel,
-        location = vec3(-804.386, 5224.654, 101.388),
-        rotation = vector3(-13, 0, 30),
+    model = Config.treeModel,
+    location = vec3(-743.369, 5376.933, 55.414),
+    rotation = vec3(0,0,0)
     },
     {
-        model = Config.treeModel,
-        location = vec3(-792.620, 5242.841, 97.755),
-        rotation = vector3(-13, 0, 30),
+    model = Config.treeModel,
+    location = vec3(-731.026, 5368.666, 59.563),
+    rotation = vec3(0,0,0)
     },
     {
-        model = Config.treeModel,
-        location = vec3(-802.525, 5248.996, 92.370),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-799.416, 5274.909, 88.427),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-820.394, 5279.470, 86.442),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-828.972, 5309.455, 78.549),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-801.190, 5308.997, 79.216),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-773.003, 5311.938, 77.700),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-760.557, 5279.392, 86.603),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-779.152, 5279.138, 89.500),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-743.474, 5280.318, 82.074),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-733.394, 5292.494, 76.040),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-745.261, 5307.534, 74.648),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-715.083, 5273.432, 75.678),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-689.934, 5304.970, 69.995),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-715.598, 5327.916, 71.052),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-718.386, 5355.340, 64.214),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-741.768, 5349.764, 61.976),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-760.626, 5355.654, 56.485),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-761.639, 5375.658, 52.207),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-749.996, 5397.920, 45.290),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-733.386, 5411.456, 50.028),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-712.673, 5404.018, 52.056),
-        rotation = vector3(-13, 0, 30),
-    },
-    {
-        model = Config.treeModel,
-        location = vec3(-680.099, 5389.764, 54.216),
-        rotation = vector3(-13, 0, 30),
+    model = Config.treeModel,
+    location = vec3(-718.712, 5363.774, 61.941),
+    rotation = vec3(0,0,0)
     },
 }
 
 Config.AddBlip = true
 Config.BlipSettings = {
-    coords = vec3(1200.505, -1276.669, 35.225),
-    id = 77,
-    display = 4,
-    scale = 0.8,
-    colour = 25,
-    name = 'Lumberjack'
+    {
+        coords = vec3(1200.505, -1276.669, 35.225),
+        id = 77,
+        display = 4,
+        scale = 0.8,
+        colour = 25,
+        name = 'Lumberjack'
+    },
+    {
+        coords = vec3(-725.893, 5376.885, 58.279),
+        id = 210,
+        display = 4,
+        scale = 0.8,
+        colour = 25,
+        name = 'Forest'
+    }
+
 }
